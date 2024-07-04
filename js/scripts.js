a04d7d95033be5ec7de21080bff2547158f8d856
frenzy128 <hashansasanka24@gmail.com>
2024-07-04 23:19:07 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Leaflet Map
+
+const map = L.map('map').setView([6.3340647, 80.8538325], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
